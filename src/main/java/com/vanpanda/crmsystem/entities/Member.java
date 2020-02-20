672013a38dc59c6a94cafd9b3946bc726e91f455
David Chou <mrycchou@gmail.com>
2020-02-20 13:35:47 -0800

    Add Member entity

---
+++ src/main/java/com/vanpanda/crmsystem/entities/Member.java
@@ -0,0 +1,116 @@
+package com.vanpanda.crmsystem.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Members")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "CHAR(100)")
+    private String lastName;
+
+    @Column(columnDefinition = "CHAR(100)")
+    private String firstName;
+
+    @Column(columnDefinition = "CHAR(250)")
+    private String email;
+
+    @Column(columnDefinition = "INT")
+    private int points;
+
+    @ManyToOne
+    private Membership membership;
+
+    @Column(columnDefinition = "CHAR(100)")
+    private String phone;
+
+    @Column(columnDefinition = "CHAR(1000)")
+    private String address;
+
+    @Column(columnDefinition = "DATE")
+    private Date dateOfBirth;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public Membership getMembership() {
+        return membership;
+    }
+
+    public void setMembership(Membership membership) {
+        this.membership = membership;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
